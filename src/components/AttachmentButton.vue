95d7b4dd03fd17001930e724ef81374dac775e58
Eki <eki@ieki.xyz>
2021-01-25 17:45:51 +0800

    ✨feat(attachment button): add tooltip

--- src/components/AttachmentButton.vue
+++ src/components/AttachmentButton.vue
@@ -1,15 +1,22 @@
 <template>
-  <v-btn
-    color="blue darken-2"
-    dark
-    fab
-    target="_blank"
-    :href="url"
-  >
-    <v-icon>
-      mdi-cloud-download
-    </v-icon>
-  </v-btn>
+  <v-tooltip top>
+    <template #activator="{ on, attrs }">
+      <v-btn
+        color="blue darken-2"
+        dark
+        fab
+        target="_blank"
+        :href="url"
+        v-bind="attrs"
+        v-on="on"
+      >
+        <v-icon>
+          mdi-cloud-download
+        </v-icon>
+      </v-btn>
+    </template>
+    <span>Download Attachment</span>
+  </v-tooltip>
 </template>
 
 <script>
